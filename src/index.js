90ceb6dd618b7de520fd1b5e8fb779673eb9430b
jugosack <jugoslav_ackoski@yahoo.com>
2023-02-06 22:09:18 +0100

    Add footer-group task #16

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+
+
+import Github from './images/github.svg';
+
+const loadGithubIcon = () => {
+  const github = document.getElementById('github');
+  github.src = Github;
+  github.alt = 'github';
+  github.setAttribute('class', 'github-icon');
+  const github1 = document.getElementById('github1');
+  github1.src = Github;
+  github1.alt = 'github';
+  github1.setAttribute('class', 'github-icon');
+  const github2 = document.getElementById('github2');
+  github2.src = Github;
+  github2.alt = 'github';
+  github2.setAttribute('class', 'github-icon');
+};
+
+
+window.onload = () => {
+ 
+  loadGithubIcon();
+};
